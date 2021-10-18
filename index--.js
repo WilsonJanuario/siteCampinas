1d98068eafdae871c5b350997cdabfee5c981739
WilsonJanuario <57508636+WilsonJanuario@users.noreply.github.com>
2021-10-18 20:32:16 -0300

    Add files via upload

---
+++ index--.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { ContainerPage, TitlePage } from "../../components/Main.js";
+
+const Page = () => {
+    return (
+        <ContainerPage>
+            
+            <TitlePage> Pagina sobre...cert</TitlePage>
+            
+ 
+ 
+
+        </ContainerPage>
+            
+        
+    );
+
+}
+
+export default Page;
